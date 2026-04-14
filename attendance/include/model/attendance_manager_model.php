2a04129e5f8da3dbdb0729f2b953383ad38d6809
ithachioji08 <146053692+ithachioji08@users.noreply.github.com>
2026-04-14 11:56:41 +0900

    Delete attendance directory
    本来ない位置のattendance直下のファイルを削除

--- attendance/include/model/attendance_manager_model.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once 'attendance_common_model.php';
-require_once 'attendance_updateApp_model.php';
-
-
--- attendance/include/view/attendance_header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title><?php echo $title?></title>
-	<link rel="stylesheet" href="attendance.css" />
-	<?php echo $sclipt ?>
-</head>
-<body>
